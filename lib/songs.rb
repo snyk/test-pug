79980ba19a231282d3fb7331774151f499600a66
Dominic Bou-Samra <dominic.bou.samra@gmail.com>
2012-02-07 12:02:32 +1000

    Added more songs

--- lib/songs.rb
+++ lib/songs.rb
@@ -29,7 +29,32 @@ class Songs
         :artist => "Queen",
         :track  => "Don't Stop Me Now",
         :url    => "http://www.youtube.com/watch?v=HgzGwKwLmgM"
-      }
+      },
+			{
+				:artist => "Gotye feat. Kimbra",
+				:track  => "Somebody I Use To Know",
+				:url 		=> "http://www.youtube.com/watch?v=8UVNT4wvIGY"
+			},
+			{
+				:artist => "Oasis",
+				:track  => "Wonderwall",
+				:url 		=> "http://www.youtube.com/watch?v=6hzrDeceEKc"
+			},
+			{
+				:artist => "Bob Dylan",
+				:track  => "Like A Rolling Stone",
+				:url 		=> "http://www.youtube.com/watch?v=hk3mAX5xdxo"
+			},
+			{
+				:artist => "Rolling Stones",
+				:track  => "I Can't Get No Satisfaction",
+				:url 		=> "http://www.youtube.com/watch?v=3a7cHPy04s8"
+			},
+			{
+				:artist => "John Lennon",
+				:track  => "Imagine",
+				:url 		=> "http://www.youtube.com/watch?v=9Q0Eyw3l3XM"
+			}
     ]
   end
 end
