baff723602ef7e37b919472a7f22f7876c1b8f12
Andrew Vos <andrew.vos@gmail.com>
2012-02-12 22:24:42 +0000

    Automatically update the songs from the json file every 10 seconds so I don't have to do a fucking `heroku restart` every time

--- lib/songs.rb
+++ lib/songs.rb
@@ -2,13 +2,20 @@ require "httparty"
 require "json"
 
 class Songs
+  CACHE_TIME = 10
+
   def self.all
-    @songs ||= download_songs
+    if @songs.nil? || Time.now > (@last_download_time + CACHE_TIME)
+      @last_download_time = Time.now
+      @songs = download_songs
+    end
+    @songs
   end
 
   private
 
     def self.download_songs
+      puts "downloading"
       @songs = JSON.parse(HTTParty.get("https://raw.github.com/AndrewVos/fuckingawesomesongs.com/master/songs.json").body)
     end
 end
